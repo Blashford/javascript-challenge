5573210d058fa40ab93492e0812e8dc9c1782fb3
Blashford <ashford.blake@gmail.com>
2020-12-24 11:41:32 -0800

    finished level 1, may make my own custom html though

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var form = d3.select("form");
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+form.on("submit", runEnter);
+button.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+
+    var input = d3.select("#datetime");
+    var inputValue = input.property("value");
+
+    var filteredTable = tableData.filter(dicty => {
+        return dicty.datetime === inputValue;
+    });
+    console.log(filteredTable)
+
+    filteredTable.forEach(dicty =>{
+      var row = tbody.append("tr");
+      Object.entries(dicty).forEach(([key, value]) => {
+          row.append("td").text(value);
+      });
+    });
+};
